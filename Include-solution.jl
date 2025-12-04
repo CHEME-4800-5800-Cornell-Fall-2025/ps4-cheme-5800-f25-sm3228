4117a54f294f1365891995f9e8b44aee04d06aa7
sm3228 <sm3228@cornell.edu>
2025-12-04 17:10:33 -0500

    Update Include-solution.jl

--- Include-solution.jl
+++ Include-solution.jl
@@ -6,7 +6,7 @@ const _PATH_TO_SOLN_SRC = joinpath(_ROOT, "solution");
 # if we are missing any packages, install them -
 using Pkg;
 if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
-    Pkg.add(path="https://github.com/varnerlab/VLDataScienceMachineLearningPackage.jl.git")
+    Pkg.add(path="https://gaithub.com/varnerlab/VLDataScienceMachineLearningPackage.jl.git")
     Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
 end
 
